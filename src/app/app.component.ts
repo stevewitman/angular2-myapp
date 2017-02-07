5489daee7575ab9847dbdbd569e50fb157046c69
Steve Witman <stevewitman@gmail.com>
2017-02-06 21:41:51 -0700

    Modify root component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  name = 'John Doe';
+  email = 'john@mail.com'
+  address = {
+    street: '12 Main St',
+    city: 'Golden',
+    state: 'CO',
+  }
 }
